4384464570b962495dfbdf8b298deecec84cc915
druzy <jonathan.legreneur@free.fr>
2016-09-19 20:29:57 +0200

    v1.0.3'

--- lib/compilator.rb
+++ lib/compilator.rb
@@ -21,10 +21,13 @@ module Compilator
     def notify_action(view,action,kwargs={})
       
       if action == :key_pressed || action == :button_pressed
-        model.last_key_pressed = kwargs[:key]
+        @model.last_key_pressed = kwargs[:key]
       
       elsif action == :key_enter_pressed
         notify_action(view,:button_equal_pressed,kwargs)
+        
+      elsif action == :key_delete_pressed
+        @model.delete = true
               
       elsif action == :button_equal_pressed
         numerateur = 0
@@ -32,10 +35,10 @@ module Compilator
           numerateur = numerateur+model.score[c]
         end
         denominateur = kwargs[:value].size*5
-        model.result = numerateur.to_s+"/"+denominateur.to_s+" => "+(numerateur*20.to_f/denominateur).round(2).to_s+"/20"
+        @model.result = numerateur.to_s+"/"+denominateur.to_s+" => "+(numerateur*20.to_f/denominateur).round(2).to_s+"/20"
         
       elsif action == :button_raz_pressed
-        model.raz = true
+        @model.raz = true
         
       elsif action == :close_window 
         view.close
@@ -45,7 +48,7 @@ module Compilator
   
   class CompilatorModel < Druzy::MVC::Model
     
-    attr_accessor :score, :last_key_pressed, :result
+    attr_accessor :score, :last_key_pressed, :result, :delete
     
     
     def initialize
@@ -54,6 +57,7 @@ module Compilator
       @last_key_pressed = nil
       @result = nil
       @raz = false
+      @delete = false
     end
     
     def last_key_pressed=(last_key_pressed)
@@ -70,23 +74,37 @@ module Compilator
       old, @raz = @raz, raz
       fire_property_change(Druzy::MVC::PropertyChangeEvent.new(self,"raz",old,@raz))
     end
+    
+    def delete=(delete)
+      old, @delete = @delete, delete
+      fire_property_change(Druzy::MVC::PropertyChangeEvent.new(self,'delete',old,@delete))
+    end
   end
   
   class CompilatorView < Druzy::MVC::View
   
     def initialize(controller)
       super(controller)
-      #Tk::Tile::Style.theme_use "classic"
       
       @root = TkRoot.new
       @root.title = 'Compilator'
       @root.bind('Key') do |event|
         val = event.valid_fields['keysym'].downcase
-        
         if val == 'return'
           Thread.new do
             @controller.notify_action(self, :key_enter_pressed, :value => @entry_variable.value)
           end
+          
+        elsif val == 'escape'
+          Thread.new do
+            @controller.notify_action(self, :button_raz_pressed)
+          end
+          
+        elsif val == 'delete'
+          Thread.new do
+            @controller.notify_action(self, :key_delete_pressed)
+          end
+          
         elsif @controller.model.score.has_key?(val)
           Thread.new do
             @controller.notify_action(self,:key_pressed, :key => val)
@@ -97,23 +115,22 @@ module Compilator
       
       @window = Tk::Tile::Frame.new(@root)
       
+      @font_button = TkFont.new("size" => 24)
+      
       @entry_variable = TkVariable.new
       @entry = Tk::Tile::Entry.new(@window)
       @entry.justify = 'right'
       @entry.validate = 'key'
-      @entry.textvariable = @entry_variable 
+      @entry.textvariable = @entry_variable
       @entry.validatecommand do |valid|
-        if valid.action == 1
-         false
-       else
-         true
-       end          
+        false        
       end
       
       @button_r = Tk::Tile::Button.new(@window)
       @button_r['text'] = 'R'
-      Tk::Tile::Style.configure('Red.TButton', {"foreground" => 'red'  })
+      Tk::Tile::Style.configure('Red.TButton', {"foreground" => 'red', 'font' => @font_button})
       @button_r['style'] = 'Red.TButton'
+      @button_r['width'] = 1
       @button_r.bind('ButtonPress') do
         Thread.new do
           @controller.notify_action(self,:button_pressed, :key => 'r')
@@ -122,8 +139,9 @@ module Compilator
             
       @button_o = Tk::Tile::Button.new(@window)
       @button_o['text'] = 'O'
-      Tk::Tile::Style.configure('Orange.TButton', {"foreground" => 'orange'  })
+      Tk::Tile::Style.configure('Orange.TButton', {"foreground" => 'orange', 'font' => @font_button})
       @button_o['style'] = 'Orange.TButton'
+      @button_o['width'] = 1
       @button_o.bind('ButtonPress') do
         Thread.new do
           @controller.notify_action(self,:button_pressed, :key => 'o')
@@ -132,7 +150,8 @@ module Compilator
       
       @button_j = Tk::Tile::Button.new(@window)
       @button_j['text'] = 'J'
-      Tk::Tile::Style.configure('Yellow.TButton', {"foreground" => 'yellow'  })
+      @button_j['width'] = 1
+      Tk::Tile::Style.configure('Yellow.TButton', {"foreground" => 'yellow', 'font' => @font_button})
       @button_j['style'] = 'Yellow.TButton'
       @button_j.bind('ButtonPress') do
         Thread.new do
@@ -142,7 +161,8 @@ module Compilator
       
       @button_c = Tk::Tile::Button.new(@window)
       @button_c['text'] = 'C'
-      Tk::Tile::Style.configure('Lime.TButton', {"foreground" => 'lime green'  })
+      @button_c['width'] = 1
+      Tk::Tile::Style.configure('Lime.TButton', {"foreground" => 'lime green', 'font' => @font_button})
       @button_c['style'] = 'Lime.TButton'
       @button_c.bind('ButtonPress') do
         Thread.new do
@@ -152,7 +172,8 @@ module Compilator
       
       @button_v = Tk::Tile::Button.new(@window)
       @button_v['text'] = 'V'
-      Tk::Tile::Style.configure('Green.TButton', {"foreground" => 'green'  })
+      @button_v['width'] = 1
+      Tk::Tile::Style.configure('Green.TButton', {"foreground" => 'green', 'font' => @font_button})
       @button_v['style'] = 'Green.TButton'
       @button_v.bind('ButtonPress') do
         Thread.new do
@@ -163,7 +184,8 @@ module Compilator
       @button_e = Tk::Tile::Button.new(@window)
       @button_e['text'] = 'E'
       @button_e['underline'] = 0
-      Tk::Tile::Style.configure('Underline.TButton', {"foreground" => 'green'})
+      @button_e['width'] = 1
+      Tk::Tile::Style.configure('Underline.TButton', {"foreground" => 'green', 'font' => @font_button})
       @button_e['style'] = 'Underline.TButton'
       @button_e.bind('ButtonPress') do
         Thread.new do
@@ -173,6 +195,9 @@ module Compilator
       
       @button_raz = Tk::Tile::Button.new(@window)
       @button_raz['text'] = 'RàZ'
+      @button_raz['width'] = 1
+      Tk::Tile::Style.configure('Medium.TButton', {'font' => TkFont.new('size' => 12)})
+      @button_raz['style'] = 'Medium.TButton'
       @button_raz.bind('ButtonPress') do
         Thread.new do
           @controller.notify_action(self,:button_raz_pressed)
@@ -181,6 +206,9 @@ module Compilator
       
       @button_equal = Tk::Tile::Button.new(@window)
       @button_equal['text'] = '='
+      Tk::Tile::Style.configure('Big.TButton', {'font' => TkFont.new('size' => 24)})
+      @button_equal['style'] = 'Big.TButton'
+      @button_equal['width'] = 1
       @button_equal.bind('ButtonPress') do
         Thread.new do
           @controller.notify_action(self,:button_equal_pressed, :value => @entry_variable.value)
@@ -188,7 +216,10 @@ module Compilator
       end
       
       @label_result = Tk::Tile::Label.new(@window)
-      @label_result.justify('right')
+      @label_result.justify = 'right'
+      Tk::Tile::Style.configure('Big.TLabel',{'font' => TkFont.new('size' => 12)})
+      @label_result['style'] = 'Big.TLabel'
+      
       
       #ajout
       TkGrid.columnconfigure( @root, 0, :weight => 1 )
@@ -217,7 +248,7 @@ module Compilator
       @button_raz.grid(:column => 0, :row => 3, :sticky => 'ewns')
       @button_equal.grid(:column => 1, :row => 3, :columnspan => 2, :sticky => 'ewns')
       
-      @label_result.grid(:column => 0, :row => 4, :columnspan => 3, :sticky => 'ew')
+      @label_result.grid(:column => 0, :row => 4, :columnspan => 3, :sticky => 'ews')
       
     end
     
@@ -229,14 +260,24 @@ module Compilator
   
     def property_change(event)
       if event.property_name == "last_key_pressed"
-        @entry_variable.set_value(@entry_variable.value+event.new_value)
+        Thread.new do
+          @entry_variable.set_value(@entry_variable.value+event.new_value)
+        end
         
       elsif event.property_name == 'result'
-        
-        @label_result.text = event.new_value.to_s 
+        Thread.new do
+          @label_result.text = event.new_value.to_s 
+        end
         
       elsif event.property_name == 'raz'
-        @entry_variable.set_value('')
+        Thread.new do
+          @entry_variable.set_value('')
+        end
+        
+      elsif event.property_name == 'delete'
+        Thread.new do
+          @entry_variable.set_value(@entry_variable.value[0..-2])
+        end
       end
     end
   
--- lib/compilator/version.rb
+++ lib/compilator/version.rb
@@ -1,3 +1,3 @@
 module Compilator
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 end
